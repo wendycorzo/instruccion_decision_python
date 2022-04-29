53cb02f6bfd1571b10e9ae9e1c89e00ca97ba876
wendycorzo <wendyvargas@colegioguanenta.edu.co>
2022-04-29 06:33:44 -0500

    Desarrollo: diagrama y programa realizados

---
+++ ejercicio1_mayor_edad/README.md
@@ -0,0 +1,7 @@
+# Ejercicio 1:
+
+## Programa para verificar si una persona es mayor de edad 
+
+## DISEÑO 
+
+!(Diagrama de flujo)(diagrama.png "Diagrama de flujo")
---
+++ ejercicio1_mayor_edad/mayor_edad.py
@@ -0,0 +1,15 @@
+" " "Ejercicio 1:
+Programa para verificar si una persona es mayor de edad" " "
+
+print ("-------------------------------------------")
+print ("-------MAYOR DE EDAD-----------")
+print ("-------------------------------------------")
+
+# input 
+edad =int(input("Digite la edad: "))
+
+# processing 
+if edad >= 18:
+    print("La persona es MAYOR DE EDAD ")
+else:
+        print("La persona es MENOR DE EDAD")
